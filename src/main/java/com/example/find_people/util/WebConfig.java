3d01d01c238987d3793034583dc81b73cd77cfae
한지선 <seonhannn@gmail.com>
2025-05-31 12:27:03 +0900

    local test

---
+++ src/main/java/com/example/find_people/util/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.find_people.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 API 경로 허용
+                .allowedOrigins("http://127.0.0.1:5500") // 또는 "*"로 전체 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true); // 인증정보 포함 허용 시 true
+    }
+}
+
