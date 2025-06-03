521f0451681f288d8f7d4dddcfd0697a8684582c
한지선 <seonhannn@gmail.com>
2025-06-04 03:31:48 +0900

    cors 설정

--- src/main/java/com/example/find_people/util/WebConfig.java
+++ src/main/java/com/example/find_people/util/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로 허용
-                .allowedOrigins("http://127.0.0.1:5500") // 또는 "*"로 전체 허용
+                .allowedOrigins("http://127.0.0.1:5500", "https://find-people-f3c41.web.app/") // 또는 "*"로 전체 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 인증정보 포함 허용 시 true
